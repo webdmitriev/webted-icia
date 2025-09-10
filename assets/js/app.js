2845a7708d4ace4fa78ad7100c47b3ba12592fbf
webdmitriev <webdmitriev@gmail.com>
2025-09-10 15:38:06 +0300

    block footer is ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+  // current date
+  if (document.getElementById('current-date')) { document.getElementById('current-date').setAttribute("data-date", "©" + new Date().getFullYear()) }
+
+  // accordion
   document.querySelectorAll('.accordion .panel').forEach(panel => {
     panel.addEventListener('click', () => {
       document.querySelectorAll('.accordion .panel').forEach(p => {
