bc73206027847b3c86bb86fd76e87f498d5ee927
webdmitriev <webdmitriev@gmail.com>
2025-09-10 15:07:23 +0300

    block research is ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  const payment = document.querySelectorAll('.progress-content');
-  if (payment) {
-    payment.forEach(el => {
-      const line = el.querySelector('.progress-line')
-      const dataStart = +line.getAttribute('data-start')
-      const dataEnd = +line.getAttribute('data-end')
+  document.querySelectorAll('.accordion .panel').forEach(panel => {
+    panel.addEventListener('click', () => {
+      document.querySelectorAll('.accordion .panel').forEach(p => {
+        p.classList.remove('active');
+      });
 
-      const result = Math.floor(dataStart * 100 / dataEnd);
-
-      if (line.querySelector(".progress-line__real")) { line.querySelector(".progress-line__real").style.width = result + '%'; }
-    })
-  }
-
-  $('body').on('click', '.popup-close, .overlay', function () {
-    $('.popup, .overlay').removeClass('active');
-    $('.popup-video video').attr('src', '');
-  })
-
-  // плавный скролл
-  $("a.ancLinks").click(function () {
-    console.log('ddsas');
-    var elementClick = $(this).attr("href");
-    var destination = $(elementClick).offset().top;
-    $("html,body").animate({ scrollTop: destination }, 1100);
-    return false;
+      panel.classList.add('active');
+    });
   });
 
-  $(".btn-popup-pdf").click(function () {
-    $(".popup").removeClass("active");
-    $(".popup-pdf").addClass("active");
-    var iframeSRC = "/vendors/pdfjs/web/viewer.html?file=" + encodeURIComponent(this.dataset.src);
-    $(".popup-pdf--iframe").attr("src", iframeSRC);
-    $(".overlay").addClass("active");
-  });
 
-  $(".btn-popup-subscr").click(function () {
-    $(".popup-subscr").addClass("active");
-    $(".overlay").addClass("active");
-  });
 
 });
